7ddbceeeaa40431f6669a46d9ad5fc64b28d1a59
Aleksander-Szopny <szopny.aleksander@gmail.com>
2021-04-18 01:36:07 +0200

    lab4

--- MobileApp/lab4/PeopleStoreApp/PeopleStoreApp/PeopleStoreApp/App.xaml.cs
+++ MobileApp/lab4/PeopleStoreApp/PeopleStoreApp/PeopleStoreApp/App.xaml.cs
@@ -7,13 +7,13 @@ namespace PeopleStoreApp
 {
     public partial class App : Application
     {
-        private const string API_URL = "http://192.168.0.106:5000/api";
+        
         public App()
         {
             InitializeComponent();
-            var client = RestEase.RestClient.For<IPeopleClient>(API_URL);
+            
 
-            MainPage = new MainPage(client);
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
